366940b906291f1f1afd524c3c27b4c522d52ee0
Parker D Van Every <pdvanevery@gmail.com>
2023-05-18 21:46:21 -0700

    running into issue with token validation

--- bot.py
+++ bot.py
@@ -1,32 +1,14 @@
-#import discord and commands
-from dotenv import load_dotenv
 import discord
-from discord.ext import commands
-import random
+DISCORD_TOKEN = 'MTEwODkyODQyODU3ODcwOTU5NQ.G2VeOK.yRlplAb_OIDOdOioOp8S4jwOT4RTOyWfAClPTw'
 
-load_dotenv()
-
-import os
-
-bot_token = os.getenv('BOT_TOKEN')
-
-#set intents for bot
+#define intents
 intents = discord.Intents.default()
-intents.typing = True
-intents.presences = True
-intents.messages = True
-intents.guilds = True
-intents.reactions = True
+intents.message_content = True
+intents.members = True
 
-#create instance of bot and the command prefix
-bot = commands.Bot(command_prefix='!', intents=intents)
+#create client object
+client = discord.Client(intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send('Hello! How can I assist you?')
-
-bot.run(bot_token)
+if __name__ == '__main__':
+    #start the bot
+    client.run(DISCORD_TOKEN)
---
+++ responses.py
@@ -0,0 +1,17 @@
+import random
+
+def handle_response(message) -> str: 
+    #this makes it so user message can be in lower case and it will read
+    p_message = message.lower()
+
+    if p_message == 'hello':
+        return 'Hey there!'
+
+    if p_message == 'roll':
+        return str(random.randint(1,6))
+
+    if p_message == '!help':
+        #the `` creates a code block on discord
+        return '`This is a message you can modify`'
+
+    #return "I don't know what you said" if you want it to respond this way when one of the above isn't chosen

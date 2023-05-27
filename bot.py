a22d06b2ee82116069a3d3b012a777c2116521cd
Parker D Van Every <pdvanevery@gmail.com>
2023-05-27 15:09:50 -0700

    got the bot running on a basic level and hid the discord token

--- bot.py
+++ bot.py
@@ -1,5 +1,13 @@
 import discord
-DISCORD_TOKEN = 'MTEwODkyODQyODU3ODcwOTU5NQ.G2VeOK.yRlplAb_OIDOdOioOp8S4jwOT4RTOyWfAClPTw'
+from dotenv import load_dotenv
+
+#load environment variables from the .env file
+load_dotenv()
+
+#retrieve the bot token from env variables in code
+import os
+bot_token = os.getenv('BOT_TOKEN')
+
 
 #define intents
 intents = discord.Intents.default()
@@ -9,6 +17,11 @@ intents.members = True
 #create client object
 client = discord.Client(intents=intents)
 
+#tells us that bot is up and ready
+@client.event
+async def on_ready():
+    print(f'{client.user} is running!')
+
 if __name__ == '__main__':
     #start the bot
-    client.run(DISCORD_TOKEN)
+    client.run(bot_token)

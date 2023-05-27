80b778fcfd9b067831b7e9fdb197adcf8f095d2c
Parker D Van Every <pdvanevery@gmail.com>
2023-05-27 15:37:47 -0700

    separated the bot into 2 files, with main.py being the file for running the bot. added response functionality

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import responses
 from dotenv import load_dotenv
 
 #load environment variables from the .env file
@@ -8,20 +9,46 @@ load_dotenv()
 import os
 bot_token = os.getenv('BOT_TOKEN')
 
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
 
 #define intents
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-#create client object
-client = discord.Client(intents=intents)
+def run_discord_bot():
+    #create client object
+    client = discord.Client(intents=intents)
 
-#tells us that bot is up and ready
-@client.event
-async def on_ready():
-    print(f'{client.user} is running!')
+    #tells us that bot is up and ready
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is running!')
+    
+    @client.event
+    async def on_message(message):
+        #prevents infinite loop of messages
+        if message.author == client.user:
+            return
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.content)
 
-if __name__ == '__main__':
-    #start the bot
+        print(f"{username} said: '{user_message}' ({channel})")
+
+        #lets user get private message, if ? is at position 0
+        if user_message[0] == '?':
+            #returns message from position 1 onwards
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else: 
+            await send_message(message, user_message, is_private=False)
+
     client.run(bot_token)
